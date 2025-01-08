5ea2ed66ec111c7f0f68d9e46e05dfc1b2467dfa
Jatin <jatkum893@gmail.com>
2025-01-08 14:20:02 -0800

    added workflows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,59 @@
+import { abort } from "process";
 import Image from "next/image";
+//import photo from "...public/photo.jpeg"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+export default function HomePage () {
+  return(
+    <>
+  <h1 style={{color: "green", fontSize : "50px", textAlign: "center"}}>
+    Jatin Kumar</h1>
+
+    <h2 style = {{
+      position: "absolute",
+      top: "150px",
+      left: "30px", 
+      color: "Green", fontSize: "40px", textAlign: "center"}}>About</h2>
+
+    <p style = {{
+      position: "absolute",
+      top: "200px",
+      left: "30px",
+      color: 'purple',
+      fontSize: "20px", 
+    }}>Hi there! I’m an aspiring software engineer<br></br> 
+    passionate about creating impactful and innovative solutions.<br></br>
+     Currently, I’m gaining valuable experience as an intern for <br></br>
+     a fitness app, where I’m learning the ropes and building my skills. As a <br></br>
+     second-year college student, I’m driven by my love for coding and <br></br>
+     the excitement of continuous growth.</p>
+    
+    <div style = {{ position: "relative", height: "500px"}}>
+     <Image 
+      src="/photo.jpeg"  // Path is relative to the public folder
+      alt="Description of the image"
+      width={300}
+      height={300}
+      style={{position: "absolute", top: "2px", right: "100px", border: "10px solid green"}}
+    />
+      </div>
+ 
+
+    <a href = "https://drive.google.com/file/d/1VSKx2pYx44PIbchwcF8e2--Lo1QZcCDv/view"
+    style={{fontSize: "30px",
+       textDecoration: "underline",
+      position: "absolute", 
+      top: "380px",
+      left: "25px",
+      color: "green"
+  }}
+    >Resume</a>
+
+
+    </>
+
+    
   );
+
 }
